618129ead8b57e3475fb0083af68352fcde86cb2
poohne <wnstmdqo94@naver.com>
2022-12-25 14:01:57 +0900

    feat:Ex2_5 추가

--- src/Ex2_5.java
+++ src/Ex2_5.java
@@ -1,2 +1,13 @@
 public class Ex2_5 {
+    public static void main(String arg[]) {
+        int x = 10;
+        int y = 5;
+
+        System.out.println(x + y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+
+
+    }
 }
