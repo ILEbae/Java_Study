9541e3ead657e445317fe61423de5a45a4333b9a
Baer <wnstmdqo94@naver.com>
2022-12-28 18:52:00 +0900

    feat:Ex3_4 addition

---
+++ src/Ex3_4.java
@@ -0,0 +1,14 @@
+public class Ex3_4 {
+    public static void main(String args[]){
+        int i =-10;
+        int j= +i;
+        System.out.println(j);
+
+        i = -10;
+        i = -i;
+
+        System.out.println(i);
+
+
+    }
+}
